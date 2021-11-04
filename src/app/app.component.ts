fd845e06e83dc8246be777b518c5b242dae92749
ManishGadekar <Manish.Gadekar@lntinfotech.com>
2021-11-04 18:21:37 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Title,Meta } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Trao';
+export class AppComponent implements OnInit {
+  title = 'Travel On';
+  constructor(private titleService:Title,
+    private meta:Meta){}
+  ngOnInit(){
+    this.titleService.setTitle(this.title);
+  }
 }
